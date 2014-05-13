580242b92892dad541ae6fb0150d7158212d6ca3
Matthew Behlke <mexezero@yahoo.com>
2014-05-13 17:17:38 -0400

    Add basic implementation for adding a neighbor and getting the distance to it

--- src/node.cc
+++ src/node.cc
@@ -1,12 +1,24 @@
 
 #include "node.h"
 
-Node::Node(int distance)
+namespace traffic
 {
-    this.distance = distance;
-}
-
-Node::GetDistanceFrom(Node loc)
-{
-    return distance;
+    Node::Node()
+    {
+    }
+    
+    bool Node::AddNeighbor(std::shared_ptr<Node> node, double distance)
+    {
+        neighbors[node] = distance;
+        return true;
+    }
+    
+    double Node::GetDistanceFrom(std::shared_ptr<Node> node)
+    {
+        double distance = -1;
+        
+        distance = neighbors[node];
+        
+        return distance;
+    }
 }
--- src/node.h
+++ src/node.h
@@ -2,19 +2,34 @@
 #ifndef _NODE_H
 #define _NODE_H
 
+#include <map>
+#include <memory>
+#include <string>
+
 namespace traffic
 {
     class Node
     {
         public:
-        Node(int distance);
+        // Default Constructor
+        Node();
         
-        getDistanceFrom(Node loc);
+        // Add new node to the map of neighbors
+        // Returns status as bool
+        bool AddNeighbor(std::shared_ptr<Node> node, double distance);
+        
+        // Get the distance from a node
+        // Returns distance as a double, or -1 if not a neighbor
+        double GetDistanceFrom(std::shared_ptr<Node> node);
         
         private:
-        int distance;
+        // Name of the node
+        std::string name;
         
-    }
+        // Neighboring node name and distance
+        std::map<std::shared_ptr<Node>, double> neighbors;
+        
+    };
 }
 
-#endif _NODE_H
+#endif
